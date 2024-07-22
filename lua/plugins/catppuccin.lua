4e649527dc76b25d8d496eeebd8ceef54cbbd4f8
Apollo <isguohr@qq.com>
2024-07-22 14:29:21 +0800

    catppuccin | chage line number color, comment color

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,6 +1,47 @@
 return {
   "catppuccin",
   opts = {
+    -- transparent_background = vim.g.transparent_background,
     transparent_background = true,
+    styles = {
+      comments = { "italic" },
+      conditionals = { "italic" },
+      loops = {},
+      functions = {},
+      keywords = {},
+      strings = {},
+      variables = {},
+      numbers = {},
+      booleans = {},
+      properties = {},
+      types = {},
+      operators = {},
+    },
+    -- https://github.com/catppuccin/catppuccin/blob/main/docs/style-guide.md
+    custom_highlights = function(colors)
+      return {
+        Comment = { fg = colors.teal },
+        LineNr = { fg = colors.subtext0 },
+        TabLineSel = { bg = colors.pink },
+      }
+    end,
+    native_lsp = {
+      enabled = true,
+      inlay_hints = {
+        background = true,
+      },
+      virtual_text = {
+        errors = { "italic" },
+        hints = { "italic" },
+        warnings = { "italic" },
+        information = { "italic" },
+      },
+      underlines = {
+        errors = { "underline" },
+        hints = { "underline" },
+        warnings = { "underline" },
+        information = { "underline" },
+      },
+    },
   },
 }
