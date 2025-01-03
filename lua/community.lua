b5fc40323ab3a9124052c7dfe104c6bb6c0b3b62
Apollo <isguohr@qq.com>
2025-01-03 23:44:35 +0800

    vim visual multi / mini-indentscope

--- lua/community.lua
+++ lua/community.lua
@@ -6,16 +6,37 @@
 
 ---@type LazySpec
 return {
-  "AstroNvim/astrocommunity",
-  { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+    "AstroNvim/astrocommunity",
+    { import = "astrocommunity.pack.lua" },
+    -- import/override with your plugins folder
 
-  -- colorscheme
-  { import = "astrocommunity.colorscheme.catppuccin" },
+    -- colorscheme
+    { import = "astrocommunity.colorscheme.catppuccin" },
 
-  -- project
-  { import = "astrocommunity.project.project-nvim" },
+    -- project
+    { import = "astrocommunity.project.project-nvim" },
 
-  -- java
-  { import = "astrocommunity.pack.java" },
+    -- java
+    { import = "astrocommunity.pack.java" },
+
+    -- noice
+    { import = "astrocommunity.utility.noice-nvim" },
+
+    -- distant
+    -- { import = "astrocommunity.remote-development.distant-nvim" },
+
+    -- pets
+    { import = "astrocommunity.media.pets-nvim" },
+
+    -- zen
+    { import = "astrocommunity.editing-support.zen-mode-nvim" },
+
+    -- typescript-all-in-one
+    -- { import = "astrocommunity.pack.typescript-all-in-one" },
+
+    -- vim-visual-multi
+    { import = "astrocommunity.editing-support.vim-visual-multi" },
+
+    -- indent
+    { import = "astrocommunity.indent.mini-indentscope" },
 }
---
+++ lua/plugins/mini-indentscope.lua
@@ -0,0 +1,11 @@
+return {
+    "echasnovski/mini.indentscope",
+    event = "User AstroFile",
+    opts = function()
+        return {
+            options = { try_as_border = true },
+            -- symbol = "╏",
+            symbol = "╹",
+        }
+    end,
+}
